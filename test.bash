0f048af32e12f4fc6950757ad586595dd1769b68
Koito Shinya <s22c1043ak@s.chibakoudai.jp>
2023-11-30 00:20:44 +0900

    Rename the command

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Koito Shinya
+# SPDX-Licence-Identifier: BSD-3-Clause
+
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}"= 15 ] || ng $LINENO
+
+[ "$res"= 0 ] && echo OK
+exit $res
